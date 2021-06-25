2f52cba419928929db6cc3d8bd2d307c9bd671b4
pedromoreira <jpedrostonemoreira@gmail.com>
2021-06-25 11:30:58 -0300

    Adicionado System.Data.SQLite ao projeto para gerenciarmos as alterações no banco de dados.

--- Simple_Work_Order/Simple_Work_Order/Database.cs
+++ Simple_Work_Order/Simple_Work_Order/Database.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
 
 namespace Simple_Work_Order
 {
     class Database
     {
+        private static SQLiteConnection sqlconnection;
+        public static string database = Globais.database;
+
+        
     }
 }
