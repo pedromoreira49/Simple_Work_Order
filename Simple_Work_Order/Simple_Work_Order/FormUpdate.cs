3080a19f567822e7566291238292a7333a575824
pedromoreira <jpedrostonemoreira@gmail.com>
2021-06-25 21:35:13 -0300

    Criado o método para solicitar o ID da Ordem de serviço que será usado como referência.

--- Simple_Work_Order/Simple_Work_Order/FormUpdate.cs
+++ Simple_Work_Order/Simple_Work_Order/FormUpdate.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace Simple_Work_Order
 {
@@ -15,6 +16,19 @@ namespace Simple_Work_Order
         public FormUpdate()
         {
             InitializeComponent();
+        }
+
+        public static string WorkID()
+        {
+            var workID = Interaction.InputBox("Informe o ID da Ordem de serviço para alterar: ", "Alterar Ordem de serviço");
+            if (!string.IsNullOrEmpty(workID))
+            {
+                return workID;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
