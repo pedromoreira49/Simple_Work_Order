5dd1f37261eee24fff5debea1c79388d220edec5
pedromoreira <jpedrostonemoreira@gmail.com>
2021-06-25 21:22:12 -0300

    Criado Form para conter os campos necessários para atualizar os dados pelo formulario.

---
+++ Simple_Work_Order/Simple_Work_Order/FormUpdate.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Simple_Work_Order
+{
+    partial class FormUpdate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormUpdate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormUpdate";
+            this.ShowIcon = false;
+            this.Text = "Update ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
